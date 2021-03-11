d5745164947cbb63bf5991a4c540301f3a02a6c1
Jai <Jai.Porter-Scott@efficientia.com.au>
2021-03-12 07:49:16 +0800

    set homepage

---
+++ precache-manifest.7404ae8e277d08bb88ac6173bdd731f3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5620e4be5dc06cca58ff2f7076789358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3bea32d8ef9a16ba7a",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "e27f0f53f88adce5df1c",
+    "url": "/static/css/main.0ac1ac59.chunk.css"
+  },
+  {
+    "revision": "5c3bea32d8ef9a16ba7a",
+    "url": "/static/js/2.6a8ce5af.chunk.js"
+  },
+  {
+    "revision": "34491c6de1fbcf2388878b87785c74e7",
+    "url": "/static/js/2.6a8ce5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27f0f53f88adce5df1c",
+    "url": "/static/js/main.e86a3aaa.chunk.js"
+  },
+  {
+    "revision": "f7bdf0cfc9ea0dd44285692948c2d84f",
+    "url": "/static/js/main.e86a3aaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1948da6512e239c4cd",
+    "url": "/static/js/runtime-main.f9fa5b6e.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
